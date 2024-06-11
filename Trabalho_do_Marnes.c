468c4d0bf633a7450a5b0a130faea42632c37e84
JoaoD0 <164253658+JoaoD0@users.noreply.github.com>
2024-06-11 20:09:42 -0300

    Trabalho_do_Marnes.c

--- Trabalho_do_Marnes.c
+++ Trabalho_do_Marnes.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define MAX_PRODUTOS 100
 #define MAX_NOME 50
-
 typedef struct {
     int codigo;
     char nome[MAX_NOME];
     int quantidade;
     float preco;
 } Produto;
-
 Produto estoque[MAX_PRODUTOS];
 int total_produtos = 0;
-
 void adicionarProduto() {
     if (total_produtos >= MAX_PRODUTOS) {
         printf("Estoque cheio! Não é possível adicionar mais produtos.\n");
@@ -24,22 +20,19 @@ void adicionarProduto() {
     printf("Digite o código do produto: ");
     scanf("%d", &novo_produto.codigo);
     printf("Digite o nome do produto: ");
-    scanf("%s", novo_produto.nome);
+    scanf(" %[^\n]", novo_produto.nome); // Use %[^\n] to read the entire line
     printf("Digite a quantidade do produto: ");
     scanf("%d", &novo_produto.quantidade);
     printf("Digite o preço do produto: ");
     scanf("%f", &novo_produto.preco);
-    
     estoque[total_produtos] = novo_produto;
     total_produtos++;
     printf("Produto adicionado com sucesso!\n");
 }
-
 void atualizarQuantidade() {
     int codigo, nova_quantidade;
     printf("Digite o código do produto: ");
     scanf("%d", &codigo);
-    
     for (int i = 0; i < total_produtos; i++) {
         if (estoque[i].codigo == codigo) {
             printf("Digite a nova quantidade: ");
@@ -51,7 +44,6 @@ void atualizarQuantidade() {
     }
     printf("Produto não encontrado!\n");
 }
-
 void exibirProdutos() {
     if (total_produtos == 0) {
         printf("Estoque vazio!\n");
@@ -66,12 +58,10 @@ void exibirProdutos() {
         printf("------------------------\n");
     }
 }
-
 void buscarProduto() {
     int codigo;
     printf("Digite o código do produto: ");
     scanf("%d", &codigo);
-    
     for (int i = 0; i < total_produtos; i++) {
         if (estoque[i].codigo == codigo) {
             printf("Produto encontrado:\n");
@@ -84,10 +74,8 @@ void buscarProduto() {
     }
     printf("Produto não encontrado!\n");
 }
-
 int main() {
     int opcao;
-    
     while (1) {
         printf("\nSistema de Gerenciamento de Estoque\n");
         printf("1. Adicionar produto\n");
@@ -97,7 +85,6 @@ int main() {
         printf("5. Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
-        
         switch (opcao) {
             case 1:
                 adicionarProduto();
@@ -118,6 +105,5 @@ int main() {
                 printf("Opção inválida! Tente novamente.\n");
         }
     }
-    
     return 0;
 }
